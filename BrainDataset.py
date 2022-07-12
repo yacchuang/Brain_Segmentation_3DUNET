75ccc639e0e791fe0aacc8207aa91a560ebf9cf8
yacchuang <99505282+yacchuang@users.noreply.github.com>
2022-07-12 16:11:54 -0700

    edit for brain seg in 3D

--- BrainDataset.py
+++ BrainDataset.py
@@ -9,18 +9,19 @@ Created on Tue Jul 12  2022
 import os
 from PIL import Image
 from torch.utils.data import Dataset
+import SimpleITK as sitk
+import nibabel as nib
 from LoadVisualNIFTI import read_img_nii, read_img_sitk, np_BrainImg, np_PFMaskImg
 import numpy as np
 
 
 class BrainDataset(Dataset):
     def __init__(self, image_dir, mask_dir, transform=None):
-        read_img_sitk(self)
-        read_img_nii(self)
         self.image_dir = image_dir
         self.mask_dir = mask_dir
         self.transform = transform
         self.images = os.listdir(image_dir)
+
 
     def __len__(self):
         return len(self.images)
@@ -28,9 +29,15 @@ class BrainDataset(Dataset):
     def __getitem__(self, index):
         img_path = os.path.join(self.image_dir, self.images[index])
         mask_path = os.path.join(self.mask_dir, self.images[index].replace("T1_", "PFseg_"))  # Differences of filename between images and masks
+        read_img_sitk(img_path)
+        read_img_sitk(mask_path)
+        BrainImg = read_img_nii(img_path)
+        PFMaskImg = read_img_nii(mask_path)
 
-        sitk_PFMaskImg2 = sitk.GetImageFromArray(np_PFMaskImg)
-        sitk_BrainImg2 = sitk.GetImageFromArray(np_BrainImg)
+        PFMask = sitk.GetImageFromArray(PFMaskImg)
+        BrainT1 = sitk.GetImageFromArray(BrainImg)
+        image = sitk.GetArrayFromImage(BrainT1)
+        mask = sitk.GetArrayFromImage(PFMask)
 
 
         # image = np.array(Image.open(img_path).convert("RGB"))
@@ -39,7 +46,7 @@ class BrainDataset(Dataset):
 
         if self.transform is not None:
             augmentations = self.transform(image=image, mask=mask)
-            sitk_BrainImg2 = augmentations["image"]
-            sitk_PFMaskImg2 = augmentations["mask"]
+            image = augmentations["image"]
+            mask = augmentations["mask"]
 
-        return sitk_BrainImg2, sitk_PFMaskImg2
+        return image, mask
